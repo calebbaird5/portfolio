8a197c0cc981da7a282816fd05407afb081a0352
Caleb Baird <79653447+calebbaird5@users.noreply.github.com>
2025-07-18 15:23:41 -0600

    cleanup

--- deploy-to-docs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm -rf ./docs/*
-cp -r ./.output/public/* ./docs
-cd docs
-mv _nuxt nuxt
-
-find . -type f \( -name "*.js" -o -name "*.css" -o -name "*.html" \) -exec sed -i -e 's/portfolio\/_nuxt/portfolio\/nuxt/g' {} \;
-find . -name '*-e' -type f -delete
